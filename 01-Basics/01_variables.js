af4aad996b952acf766bd58ab304932f683d29c7
ArsalanKhan-creator <105183168+ArsalanKhan-creator@users.noreply.github.com>
2023-09-27 14:53:58 +0000

    datatypes

---
+++ 01-Basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "annukhan782@gmail.com"
+var accountPassword = "12345"
+accountCity = "Peshawar"
+
+// accountId = 2  not allowed   const"constant" = remain same cant change it
+
+accountEmail = "hc.hx@gmail.com"
+accountPassword = "234234"
+accountCity = "islamabad"
+let accounstate;
+console.log(accountId);
+console.log(accountEmail);
+// table console log system you can get print in table form
+
+console.table({accountId, accountEmail, accountPassword, accountCity, accounstate});
---
+++ 01-Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodejs, not browser it only works in browers
+
+// console.log(3 + 3); console.log("Arsalan") // this not Good to write JS code readibility should be high
+
+// string data Type
+let name = ("Arsalan")
+// number data type  number = 2 to power 53
+let age = 27
+// boolean data type = true/false
+let isLoggedIn = false
+// bigint data type
+
+// null data type or standalone value/Type
+let city = null
+// undefined => value not decided yet
+let state;
+// symbol => unique data type
+
+// object
+
+console.log(typeof undefined); //undefined 
+console.log(typeof null); // object
